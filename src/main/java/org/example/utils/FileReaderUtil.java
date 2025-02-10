cd7e1de6f9182449d84770e718750392654913b9
notBigO <2003varunp@gmail.com>
2025-02-10 16:23:08 +0530

    Add initial dataset of actors in CSV format

---
+++ src/main/java/org/example/utils/FileReaderUtil.java
@@ -0,0 +1,21 @@
+package org.example.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReaderUtil {
+    public static List<String[]> readCSV(String filePath) throws IOException {
+        List<String[]> data = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                data.add(values);
+            }
+        }
+        return data;
+    }
+}
